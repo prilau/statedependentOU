904778c0473ccf73aff15c86c0d1ce306fbfbffc
Priscilla Lau <priprilau@gmail.com>
2025-01-22 22:03:56 +0100

    increase lambda sdou3

---
+++ scripts/4_plots/compareNodePosterior.R
@@ -0,0 +1,90 @@
+library(RevGadgets)
+library(tidyverse)
+tree <- read.tree("data/3_empirical/mammal_perMY_r500.tre")
+
+index_to_rev <- matchNodes(tree)
+
+dir_in = "output/3_empirical/aug_r500_3StateOrderedModel/"
+files <- list.files(dir_in)[grepl(".trees", list.files(dir_in))]
+for (file in files){
+  path = paste0(dir_in, file)
+  log <- read_tsv(path)
+  log <- as.data.frame(log$char_hist)
+  write_tsv(log, file=path, col_names = FALSE)
+}
+
+
+simmap_to_ancStates <- function(input_path, output_path){
+  #input_path <- paste0(dir_in, list.files(dir_in))
+  simmaps <- read.simmap(input_path, format="phylip")
+  
+  df_rev <- data.frame()
+  
+  for (row_num in 1:length(simmaps)){
+    simmap <- simmaps[[row_num]]
+    
+    # Iteration column
+    df_rev[row_num, 1] <- row_num-1
+    
+    for (i in 1:(length(simmap$maps))){
+      ape_node <- which(index_to_rev[,2]==i)
+      ape_edge <- which(simmap$edge[,2]==ape_node)
+      map <- simmap$maps[[ape_edge]]
+      df_rev[row_num, i+1] <- names(map[length(map)])
+    }
+    df_rev[row_num, length(simmap$maps)+2] <- names(map[1])
+  }
+  
+  header <- paste0("end_", as.character(1:(length(simmap$maps)+1)))
+  colnames(df_rev) <- c("Iteration", header)
+  write_tsv(df_rev, output_path)
+}
+
+for (i in 1:length(files)){
+  file_in <- paste0("output/3_empirical/aug_r500_3StateOrderedModel/augch_run_", i, ".trees")
+  file_out <- paste0("output/3_empirical/aug_r500_3StateOrderedModel/states_run_", i, ".log")
+  simmap_to_ancStates(file_in, file_out)
+}
+
+
+#Do this in RevBayes!
+#tree <- readTrees("data/3_empirical/mammal_perMY_r500.tre")[1]
+#files_in <- ["output/3_empirical/aug_r500_3StateOrderedModel/states_run_1.log",
+#             "output/3_empirical/aug_r500_3StateOrderedModel/states_run_2.log",
+#             "output/3_empirical/aug_r500_3StateOrderedModel/states_run_3.log",
+#             "output/3_empirical/aug_r500_3StateOrderedModel/states_run_4.log",
+#             "output/3_empirical/aug_r500_3StateOrderedModel/states_run_5.log"]
+#files_out <- ["output/3_empirical/aug_r500_3StateOrderedModel/anc_states_run_1.tre",
+#              "output/3_empirical/aug_r500_3StateOrderedModel/anc_states_run_2.tre",
+#              "output/3_empirical/aug_r500_3StateOrderedModel/anc_states_run_3.tre",
+#              "output/3_empirical/aug_r500_3StateOrderedModel/anc_states_run_4.tre",
+#              "output/3_empirical/aug_r500_3StateOrderedModel/anc_states_run_5.tre"]
+#i=1
+#for (file in files_in){
+#  anc_states = readAncestralStateTrace(file)
+#  anc_tree = ancestralStateTree(
+#  tree=tree,
+#  ancestral_state_trace_vector=anc_states,
+#  include_start_states=false,
+#  file=files_out[i],
+#  summary_statistic="MAP",
+#  reconstruction="marginal",
+#  burnin=0.1,
+#  nStates=3,
+#  site=1)
+#  i+=1
+# }
+
+ase <- processAncStates("output/3_empirical/ase_r500_3StateOrdered/anc_states_run_1.tre",
+                        state_labels=c("0"="Herbivorous",
+                                       "1"="Omnivorous",
+                                       "2"="Carnivorous"))
+aug <- processAncStates("output/3_empirical/aug_r500_3StateModel/anc_states_run_1.tre",
+                        state_labels=c("0"="Herbivorous",
+                                       "1"="Omnivorous",
+                                       "2"="Carnivorous"))
+
+
+ggplot() +
+  geom_point(aes(x = ase@data$anc_state_1_pp, y = aug@data$anc_state_1_pp))
+
--- scripts/4_plots/plot_ase_stoch_map_r500.R
+++ scripts/4_plots/plot_ase_stoch_map_r500.R
@@ -5,64 +5,6 @@ library(tidyverse)
 source("scripts/4_plots/revgadgets_StochMap.R")
 
 
-#tree <- read.tree("data/3_empirical/dummy_r6.tre")
-#index_to_rev <- matchNodes(tree)
-#
-#dir_in = "output/3_empirical/aug_tipMissingDiscrete/"
-#files <- list.files(dir_in)[grepl(".trees", list.files(dir_in))]
-#for (file in files){
-#  path = paste0(dir_in, file)
-#  log <- read_tsv(path)
-#  log <- as.data.frame(log$char_hist)
-#  write_tsv(log, file=path, col_names = FALSE)
-#}
-#
-##simmap_to_ancStates <- function(input_path, output_path){
-#input_path <- paste0(dir_in, list.files(dir_in))
-#simmaps <- read.simmap(input_path, format="phylip")
-#
-#df_rev <- data.frame()
-#
-#for (row_num in 1:length(simmaps)){
-#  simmap <- simmaps[[row_num]]
-#  
-#  # Iteration column
-#  df_rev[row_num, 1] <- row_num-1
-#  
-#  for (i in 1:(length(simmap$maps))){
-#    ape_node <- which(index_to_rev[,2]==i)
-#    ape_edge <- which(simmap$edge[,2]==ape_node)
-#    map <- simmap$maps[[ape_edge]]
-#    df_rev[row_num, i+1] <- names(map[length(map)])
-#  }
-#  df_rev[row_num, length(simmap$maps)+2] <- names(map[1])
-#}
-#header <- paste0("end_", as.character(1:(length(simmap$maps)+1)))
-#colnames(df_rev) <- c("Iteration", header)
-#write_tsv(df_rev, output_path)
-#
-#simmap_to_ancStates("output/3_empirical/3.2a_sdOU/augch.trees", "output/3_empirical/3.2a_sdOU/anc_states.log")
-
-
-#Do this in RevBayes!
-#tree <- readTrees("data/3_empirical/dummy_r6.tre")[1]
-#
-#for (file in listFiles("output/3_empirical/aug_tipMissingDiscrete/augch/")){
-#  anc_states = readAncestralStateTrace(file)
-#  anc_tree = ancestralStateTree(
-#    tree=tree,
-#    ancestral_state_trace_vector=anc_states,
-#    include_start_states=false,
-#    file=file + "ancStates.tre",
-#    summary_statistic="MAP",
-#    reconstruction="marginal",
-#    burnin=0.1,
-#    nStates=3,
-#    site=1)
-#}
-
-
-
 #Plotting
 tree <- readTrees("data/3_empirical/mammal_perMY_r500.tre")
 t <- read.tree("data/3_empirical/mammal_perMY_r500.tre")
@@ -280,3 +222,79 @@ p3 <- plotStochMaps(tree, maps=stoc_map,
   theme(legend.position.inside = c(0.6,0.81))
 #p3
 ggsave(paste0("figures/3_empirical/ase_r500_3StateOrdered/stoch_map.pdf"), p3, width = 8, height = 6)
+
+
+
+
+
+
+
+
+
+#############
+# augmented #
+#############
+#  3-state  #
+#############
+ase <- processAncStates("output/3_empirical/aug_r500_3StateOrderedModel/anc_states_run_5.tre",
+                        state_labels=c("0"="Herbivorous", "1"="Omnivorous", "2"="Carnivorous"))
+# produce the plot object, showing MAP states at nodes.
+p7 <- plotAncStatesMAP(t = ase,
+                       #tip_labels_offset = 0.5,
+                       tip_labels = FALSE,
+                       node_color_as = "state",
+                       node_color = c("Herbivorous"="#44AA99", "Omnivorous"="#ddcc77", "Carnivorous"="#882255"),
+                       node_size = c(0.5, 2),
+                       tip_states = TRUE,
+                       #tip_states_size = c(0.01, 0.01),
+                       #tip_states_shape = 1,
+                       state_transparency = 0.7,
+                       tree_layout = "circular",
+                       #tip_labels_size = 0.5
+                       tree_color = "#bbbbbb",
+                       tree_linewidth = 0.25) +
+  # modify legend location using ggplot2
+  theme(legend.position.inside = c(0.6,0.81))
+#p1
+ggsave(paste0("figures/3_empirical/aug_r500_3StateOrderedModel/ase_map.pdf"), p7, width = 8, height = 6)
+
+
+p8 <- plotAncStatesPie(t = ase,
+                       pie_colors = c("Herbivorous"="#44AA99", "Omnivorous"="#ddcc77", "Carnivorous"="#882255"),
+                       #tip_labels_size = 1,
+                       tip_pies = TRUE,
+                       #tip_labels_offset = 0.5,
+                       node_pie_size = 0.7,
+                       tip_pie_size = 0.2,
+                       tree_layout = "circular",
+                       tip_labels = FALSE,
+                       state_transparency = 0.7,
+                       tree_color = "#bbbbbb",
+                       tree_linewidth = 0.25) +
+  # modify legend location using ggplot2
+  theme(legend.position.inside = c(0.6,0.81))
+#p2
+ggsave(paste0("figures/3_empirical/aug_r500_3StateOrderedModel/ase_pie.pdf"), p8, width = 8, height = 6)
+
+
+# p3
+simmaps <- read.simmap("output/3_empirical/aug_r500_3StateOrderedModel/augch_run_5.trees", format="phylip")
+stoc_map <- processStochMaps(tree,
+                             simmap = simmaps,
+                             states=c("0", "1", "2"))
+
+colnames(stoc_map)[6] = "Herbivorous"
+colnames(stoc_map)[7] = "Omnivorous"
+colnames(stoc_map)[8] = "Carnivorous"
+
+p9 <- plotStochMaps(tree, maps=stoc_map,
+                    tip_labels = FALSE,
+                    tree_layout = "circular",
+                    line_width=0.25,
+                    color_by = "MAP",
+                    colors = c("Herbivorous"="#44AA99",
+                               "Omnivorous"="#ddcc77",
+                               "Carnivorous"="#882255")) +
+  theme(legend.position.inside = c(0.6,0.81))
+#p3
+ggsave(paste0("figures/3_empirical/aug_r500_3StateOrderedModel/stoch_map.pdf"), p9, width = 8, height = 6)
