cdbfd178628acce9f694d44077b4d84ae7ebca96
Priscilla Lau <priprilau@gmail.com>
2025-02-21 14:46:02 +0100

    R

--- scripts/2_simulation/compareAlgorithmSpeed.R
+++ scripts/2_simulation/compareAlgorithmSpeed.R
@@ -8,6 +8,113 @@ library(tibble)
 library(ggdist)
 #source("scripts/5_miscellaneous/functions.R")
 
+sd_postorder <- function(node_index, edge, tree, continuousChar,
+                         μ, V, log_norm_factor, subedges_lengths, alpha, sigma2, theta){
+  ntip = length(tree$tip.label)
+  
+  # if is internal node
+  if (node_index > ntip){
+    
+    left_edge  = which(edge[,1] == node_index)[1] # index of left child edge
+    right_edge = which(edge[,1] == node_index)[2] # index of right child edge
+    left = edge[left_edge,2] # index of left child node
+    right = edge[right_edge,2] # index of right child node
+    
+    output_left <- sd_postorder(left, edge, tree, continuousChar,
+                                μ, V, log_norm_factor, subedges_lengths, alpha, sigma2, theta)
+    μ <- output_left[[1]]
+    V <- output_left[[2]]
+    log_norm_factor <- output_left[[3]]
+    
+    output_right <- sd_postorder(right, edge, tree, continuousChar,
+                                 μ, V, log_norm_factor, subedges_lengths, alpha, sigma2, theta)
+    μ <- output_right[[1]]
+    V <- output_right[[2]]
+    log_norm_factor <- output_right[[3]]
+    
+    
+    sub_bl_left = subedges_lengths[[left_edge]] # all subedges of left child edge
+    sub_bl_right = subedges_lengths[[right_edge]] # all subedges of right child edge
+    
+    # for the sake of readability, computation of variance, mean, and log_nf are done in separate loops
+    # 1) variance of the normal variable: this branch (v_left) and the subtree (V[left])
+    ## Is 'delta_left* exp(2.0 * alpha * bl_left)' added in each sub-edge?
+    
+    delta_left = V[left]
+    v_left = 0 # initialise v_left
+    for (i in rev(1:length(sub_bl_left))){
+      state <- names(sub_bl_left[i])
+      v_left = sigma2[[state]]/(2*alpha[[state]]) *expm1(2.0*alpha[[state]]
+                                                         *sub_bl_left[[i]])
+      delta_left = v_left + delta_left * exp(2.0 * alpha[[state]] * sub_bl_left[[i]])
+    }
+    
+    delta_right = V[right]
+    v_right = 0 # initialise v_right
+    for (i in rev(1:length(sub_bl_right))){
+      state <- names(sub_bl_right[i])
+      v_right = sigma2[[state]]/(2*alpha[[state]]) *expm1(2.0*alpha[[state]]*sub_bl_right[[i]])
+      delta_right = v_right + delta_right * exp(2.0 * alpha[[state]] * sub_bl_right[[i]])
+    }
+    
+    var_left = delta_left
+    var_right = delta_right
+    
+    # 2) mean of the normal variable
+    mean_left = μ[left]
+    for (i in rev(1:length(sub_bl_left))){
+      state <- names(sub_bl_left[i])
+      mean_left = exp(alpha[[state]]*sub_bl_left[[i]])*(mean_left - theta[[state]]) + theta[[state]]
+    }
+    
+    mean_right = μ[right]
+    for (i in rev(1:length(sub_bl_right))){
+      state <- names(sub_bl_right[i])
+      mean_right = exp(alpha[[state]]*sub_bl_right[[i]])*(mean_right - theta[[state]]) + theta[[state]]
+    }
+    
+    
+    ## compute the mean and variance of the node
+    mean_ancestor = (mean_left * var_right + mean_right * var_left) / (var_left + var_right)
+    μ[node_index] = mean_ancestor
+    var_node = (var_left * var_right) / (var_left + var_right)
+    V[node_index] = var_node
+    
+    ## compute the normalizing factor, the left-hand side of the pdf of the normal variable
+    ## this is the problem. I think in RevBayes we compute log_nf with the oldest sub-edge only
+    log_nf_left = 0
+    for (i in rev(1:length(sub_bl_left))){
+      state <- names(sub_bl_left[i])
+      log_nf_left = log_nf_left + sub_bl_left[[i]] * alpha[[state]]
+    }
+    
+    log_nf_right = 0
+    for (i in rev(1:length(sub_bl_right))){
+      state <- names(sub_bl_right[i])
+      log_nf_right = log_nf_right + sub_bl_right[[i]] * alpha[[state]]
+    }
+    
+    contrast = mean_left - mean_right
+    a = -(contrast*contrast / (2*(var_left+var_right)))
+    b = log(2*pi*(var_left+var_right))/2.0
+    #b = log(2*pi)/2.0 + log(var_left+var_right)/2.0
+    log_nf = log_nf_left + log_nf_right + a - b
+    log_norm_factor[node_index] = log_nf
+    
+    return(list(μ, V, log_norm_factor))
+  }
+  
+  
+  # if is tip
+  else{
+    species = tree$tip.label[node_index]
+    
+    μ[node_index] = as.numeric(continuousChar[[which(names(continuousChar) == species)]])
+    V[node_index] = 0.0 ## if there is no observation error
+    
+    return(list(μ, V, log_norm_factor))
+  }
+}
 sd_logL_pruning <- function(tree, continuousChar, alpha, sigma2, theta){
   ntip = length(tree$tip.label) # number of tips
   edge = tree$edge # equals tree[:edge] in Julia
